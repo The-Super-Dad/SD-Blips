79bbd50f2cc89716b094e7c868a540f8605f6b25
Super Dad <thesuperdad24@gmail.com>
2025-02-20 22:37:35 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+Config.Blips = { 
+    {
+        name = "Life Invader",     -- Name of the blip
+        coords = vector3(-1045.83, -228.45, 39.01)  , -- Coordinates of the blip
+        sprite = 77,                                   -- Blip sprite ID (icon)
+        display = 4,                                    -- Display mode for the blip (4 is always visible)
+        scale = 0.6,                                    -- Scale of the blip (size)
+        color = 1,                                      -- Blip color (3 is blue)
+        shortRange = true                               -- Blip only visible when near
+    },
+
+}
